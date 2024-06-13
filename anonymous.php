c17b7ac2127c70b47ed3d4940a9e7bd53d6c7b6b
aungNyeinChan93 <aungNyeinChan.dev@gmail.com>
2024-06-13 07:19:39 +0630

    add anonymous function

---
+++ anonymous.php
@@ -0,0 +1,25 @@
+<?php 
+
+declare(strict_types= 1);
+error_reporting(E_ALL);
+ini_set("",1);
+
+echo    "hello world";
+$foo = "bar";
+$anonymous = function () use($foo) :string{
+    return "<br> Anoymonus function $foo <br> ";
+};
+
+$test = $anonymous();
+echo $test;
+
+class Test {
+    public function __invoke(string $name): string{
+        return "I am anonymous function from call anonymous $name";
+    }
+};
+
+$test1 = new Test();
+echo $test1("test");
+
+
